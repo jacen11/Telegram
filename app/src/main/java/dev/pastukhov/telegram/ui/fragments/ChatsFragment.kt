09db262a89e81f4feceede1079f613a43a249e38
jacen <jacen19931@gmail.com>
2021-07-27 16:41:10 +0300

    BaseFragment

---
+++ app/src/main/java/dev/pastukhov/telegram/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,12 @@
+package dev.pastukhov.telegram.ui.fragments
+
+import dev.pastukhov.telegram.R
+import dev.pastukhov.telegram.ui.fragments.BaseFragment
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/dev/pastukhov/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package dev.pastukhov.telegram.ui.fragments
+
+import dev.pastukhov.telegram.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
